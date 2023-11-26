a9cae7d0387282e738576c2bd7a2951b5d25b6e1
fan-ziqi <fanziqi614@gmail.com>
2023-11-26 20:55:28 +0800

    fix: change docker name

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,4 @@ set -u
 # SCRIPTROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 # cd "${SCRIPTROOT}/.."
 
-docker build --network host -t isaacgym -f Dockerfile .
+docker build --network host -t parkour -f Dockerfile .
