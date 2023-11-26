2c5b0df340c2d488de5d5c65bf6f408ac03b322b
fan-ziqi <fanziqi614@gmail.com>
2023-11-26 20:36:27 +0800

    feat: docker support

---
+++ docker/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -u
+
+SCRIPTROOT="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if [ $# -eq 0 ]
+then
+    echo "running docker without display"
+    docker run -it --rm -v ${SCRIPTROOT}/..:/home/root/rl_ws --network=host --gpus=1 --name=isaacgym_container isaacgym /bin/bash
+else
+    export DISPLAY=$DISPLAY
+	echo "setting display to $DISPLAY"
+	xhost +
+	docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/fzq/.Xauthority:/root/.Xauthority -e DISPLAY=$DISPLAY -v ${SCRIPTROOT}/..:/home/root/rl_ws --network=host --gpus=1 --name=isaacgym_container isaacgym /bin/bash
+	# xhost -
+fi
