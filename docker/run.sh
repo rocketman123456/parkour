84caf76cd7256d6259e67a7480dac7f404f57989
fan-ziqi <fanziqi614@gmail.com>
2023-12-05 20:19:32 +0800

    fix: add docker auto init

--- docker/run.sh
+++ docker/run.sh
@@ -7,11 +7,11 @@ SCRIPTROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 if [ $# -eq 0 ]
 then
     echo "running docker without display"
-    docker run -it --rm -v ${SCRIPTROOT}/..:/home/root/rl_ws --network=host --gpus=1 --name=parkour_container parkour /bin/bash
+    docker run -it --rm -v ${SCRIPTROOT}/..:/home/root/rl_ws --network=host --gpus=1 --name=parkour_container parkour /home/root/rl_ws/setup.sh
 else
     export DISPLAY=$DISPLAY
 	echo "setting display to $DISPLAY"
 	xhost +
-	docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/fzq/.Xauthority:/root/.Xauthority -e DISPLAY=$DISPLAY -v ${SCRIPTROOT}/..:/home/root/rl_ws --network=host --gpus=1 --name=parkour_container parkour /bin/bash
+	docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/fzq/.Xauthority:/root/.Xauthority -e DISPLAY=$DISPLAY -v ${SCRIPTROOT}/..:/home/root/rl_ws --network=host --gpus=1 --name=parkour_container parkour /home/root/rl_ws/setup.sh
 	# xhost -
 fi
--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ set -u
 pip install -e isaacgym/python
 pip install -e rsl_rl
 pip install -e legged_gym
+
+/bin/bash
